8e76d08a9244ec2ba40aa3427b71bbf07492780b
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-05 16:39:06 +0530

    add solution of sum challege

---
+++ 03Javascript/Day-02/07sum.js
@@ -0,0 +1,13 @@
+let arr = [1, 2, 3, 4, 5, 6];
+
+
+function sumFactory(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++){
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+const sumOfNumbers = sumFactory(arr);
+console.log(`Sum is: ${sumOfNumbers}`);
