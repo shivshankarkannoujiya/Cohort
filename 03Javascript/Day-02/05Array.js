8f65b27406651e6a5c769277d500d4221ecd9826
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-05 15:58:58 +0530

    add array code

---
+++ 03Javascript/Day-02/05Array.js
@@ -0,0 +1,20 @@
+let fruits = ["apple", "cherry", "banana"];
+let intFruits = ["kiwi", "avacado", "dragon fruit"];
+
+// console.log(typeof fruits);
+// console.log(typeof intFruits);
+
+fruits[0] = "blueberry";
+
+// console.log(fruits[0])
+// console.log(fruits.length)
+
+fruits.push('new item');
+fruits.unshift('another new item');
+const deletedElement = fruits.pop();
+console.log(`Deleted Elements: ${deletedElements}`)
+console.log(fruits);
+
+
+
+
