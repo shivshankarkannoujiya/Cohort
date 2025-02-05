084acfb7efcdfaa7ec3500d05748055a215a47eb
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-05 15:10:45 +0530

    add solution of pizza challenge

---
+++ 03Javascript/Day-02/03pizza.js
@@ -0,0 +1,12 @@
+let numberOfGuests = 4;
+let pizzaSize;
+
+if (numberOfGuests <= 2) {
+    pizzaSize = "small"
+} else if (numberOfGuests <= 5) {
+    pizzaSize = "medium"
+} else {
+    pizzaSize = "large"
+}
+
+console.log(`pizzaSize: ${ pizzaSize }`);
