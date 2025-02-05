bb6a5db96048232c91a69280b8e00ae6a9e32fcb
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-05 14:43:58 +0530

    add basics of datatypes

---
+++ 03Javascript/Day-02/01bootcampProfile.js
@@ -0,0 +1,46 @@
+// string - "", '', ``
+let name = "John Doe";
+
+// Number - 25(integer), 25.5(float)
+let age = 25;
+
+// Boolean - true/false
+let isPaid = true;
+
+// null - Empty/nothing != 0
+let favouriteClass = null; 
+
+// undefined - Baad me dekhenge
+let homeTown;
+
+
+// Array
+let skills = ["Html", "CSS", "Javascript"];
+
+
+// console.table([name, age, isPaid, favouriteClass, homeTown, skills]);
+// console.table([skills]);
+
+
+// Object
+let studentProfile = {
+    name: "John",
+    age: 25,
+    isPaid: true,
+    favouriteClass: null,
+    skills: ["HTML", "CSS", "JS"]
+}
+
+
+console.table([typeof name, typeof age, typeof isPaid, typeof favouriteClass, typeof homeTown, typeof skills, typeof studentProfile])
+
+
+
+/*
+TODO: Recap
+null -      object
+array -     object
+object -    object
+function -  object
+undefined - undefined
+*/
