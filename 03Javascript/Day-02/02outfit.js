a1eb3aeccfbfecdb48f159772d4f6ecfbd800362
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-05 15:03:51 +0530

    add conditional code

---
+++ 03Javascript/Day-02/02outfit.js
@@ -0,0 +1,14 @@
+// server call : rainy, cloudy, sunny
+
+
+const weather = "rainy";
+
+if (weather === "rainy") {
+    console.log("It's raning, Carry Umbrella with You");
+} else if (weather === "cloudy") {
+    console.log("It's cloudy, Carry Jacket with You");
+} else {
+    console.log("It's Sunny day Today")
+}
+
+console.log(weather === "rainy");
