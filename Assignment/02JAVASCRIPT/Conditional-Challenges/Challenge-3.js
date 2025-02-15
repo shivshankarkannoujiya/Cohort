232db3a2dd5cfd9c5dfbc6eb036d9628f904baf6
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-15 11:28:09 +0530

    Add solution of Challenge-3

---
+++ Assignment/02JAVASCRIPT/Conditional-Challenges/Challenge-3.js
@@ -0,0 +1,9 @@
+function checkVotingEligibility(age) {
+    if (age >= 18) {
+        return "Eligible";
+    } else {
+        return "Not Eligible";
+    }
+}
+
+console.log(checkVotingEligibility(19));
