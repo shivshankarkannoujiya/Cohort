1e034f0af01fc7510fbb6b9eeb8affc831709163
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-16 16:59:01 +0530

    add under the hood of class

--- 03Javascript/Day-05/02class.js
+++ 03Javascript/Day-05/02class.js
@@ -14,5 +14,15 @@ class Person {
 const p1 = new Person("Raj", "Singh");
 const p2 = new Person("Anirudh", "Jwala");
 
-console.log(p1.getFullName());
-console.log(p2.getFullName());
+// console.log(p1.getFullName());
+// console.log(p2.getFullName());
+
+// console.log(Person.prototype);
+// console.log(p1.__proto__);
+
+
+// TODO: Under the hood
+// Working like Object literal
+// Code is clean: Syntatic Suger
+
+// p1.__proto__ = Person.prototype
