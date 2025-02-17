fe55de9797a6495a40d88730e437fd9986f4cd1c
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-17 11:38:37 +0530

    add example of first class funtion

--- Problems/03Prototype/01que.js
+++ Problems/03Prototype/01que.js
@@ -13,3 +13,26 @@ function greet(name) {
 
 //TODO: Tell the Technical Name
 
+// Function Declaration
+function add(a, b) {
+    return a + b;
+}
+
+// Function Expression
+const substract = function (a, b) {
+    return a - b;
+}
+
+// Arrow function
+const multiply = (a, b) => a * b;
+
+
+// TODO: Tell the Behaviour
+
+// First Class Function
+function applyOperation(a, b, operation) {
+    return operation(a, b);
+}
+
+const result = applyOperation(4, 2, (x, y) => x / y);
+console.log(result);
