7bb1feaff7038764cff206c56d5d7fa3545524a9
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-17 11:28:48 +0530

    Extend Funtion prototype

---
+++ Problems/03Prototype/01que.js
@@ -0,0 +1,15 @@
+Function.prototype.describe = function () {
+    console.log(`Function name is: ${this.name}`);
+}
+
+function greet(name) {
+    console.log(`My name is: ${name}`);
+}
+
+
+// Function name is: greet
+// greet.describe();
+
+
+//TODO: Tell the Technical Name
+
