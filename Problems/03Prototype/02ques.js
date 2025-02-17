43c9c9b9df8f6999e4cad28fd31b5f9a1375a02e
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-17 11:50:11 +0530

    add scope and tiffin concept

---
+++ Problems/03Prototype/02ques.js
@@ -0,0 +1,24 @@
+// TODO: Tell the behaviour of Code Snippets
+
+function createCounter() {
+    let count = 0;
+    return function () {
+        count++;
+        return count;
+    }
+}
+// Why insider function is able to access the `count`
+// Every properties of Parent is of Child
+// Tiffin concept
+// Jab ek function return hota hai then, tiffin lekar jata hai, tiffin me kya hota hai?
+// Jo kucch bahar hota hai wo lekar jata hai
+
+function onef() {
+    const myName = `Abhishek`;
+}
+
+// console.log(myName);
+// ERROR: myName is not defined
+// myName: out of scope
+// No Scope Out of the function
+
