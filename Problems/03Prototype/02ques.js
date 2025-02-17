9244de6c3c08d273014f041544f16880a2330f81
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-17 11:54:40 +0530

    Execute the function

--- Problems/03Prototype/02ques.js
+++ Problems/03Prototype/02ques.js
@@ -13,6 +13,10 @@ function createCounter() {
 // Jab ek function return hota hai then, tiffin lekar jata hai, tiffin me kya hota hai?
 // Jo kucch bahar hota hai wo lekar jata hai
 
+const counter = createCounter(); // return function: counter ==> function
+console.log(counter()); // Execute: counter()
+
+
 function onef() {
     const myName = `Abhishek`;
 }
