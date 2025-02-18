9c6088a161f8f808a17f5d036a3ce9d0b5a333c9
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-02-18 17:41:28 +0530

    add rendering feature and fixes some css and html

--- Assignment/DOM/Review Collector/script.js
+++ Assignment/DOM/Review Collector/script.js
@@ -1,47 +1,54 @@
-const textArea = document.getElementById('textBox');
-const submitButton = document.getElementById('submitReviewBtn');
-const reviewContainer = document.getElementById('reviewContainer');
+document.addEventListener('DOMContentLoaded', () => {
+
+    const textArea = document.getElementById('textBox');
+    const submitButton = document.getElementById('submitReviewBtn');
+    const reviewContainer = document.getElementById('reviewContainer');
 
 
-let reviews = [];
+    let reviews = JSON.parse(localStorage.getItem("review")) || [];
+    reviews.forEach(review => renderReview(review));
 
-submitButton.addEventListener('click', function () {
+    submitButton.addEventListener('click', function () {
 
-    const textAreaValue = textArea.value.trim();
-    if (!textAreaValue) return; 
+        const textAreaValue = textArea.value.trim();
+        if (!textAreaValue) return; 
 
-    const newReview = {
-        id: Date.now(),
-        heading: `Client Review`,
-        text: textAreaValue,
-        date: new Date().toISOString()
+        const newReview = {
+            id: Date.now(),
+            heading: `Review from a Learner`,
+            text: textAreaValue,
+            date: new Date().toISOString()
+        }
+
+        reviews.push(newReview);
+        saveReview();
+        renderReview(newReview);
+        
+        textArea.value = ''
+        console.log(reviews);
+    })
+
+
+    function renderReview(review) {
+
+        console.log(review)
+        const reviewCard = document.createElement('div');
+        reviewCard.classList.add('card');
+
+        const h2 = document.createElement('h2');
+        h2.textContent = review.heading;
+
+        const reviewText = document.createElement('p');
+        reviewText.textContent = review.text;
+
+        reviewCard.appendChild(h2);
+        reviewCard.appendChild(reviewText);
+
+        reviewContainer.prepend(reviewCard);
     }
 
-    reviews.push(newReview);
-    saveReview();
-    console.log(reviews);
 
-    const reviewCard = document.createElement('div');
-    reviewCard.classList.add('card');
-
-    const h2 = document.createElement('h2');
-    h2.textContent = newReview.heading;
-
-    const reviewText = document.createElement('p');
-    reviewText.textContent = newReview.text;
-
-    reviewCard.appendChild(h2);
-    reviewCard.appendChild(reviewText);
-
-    reviewContainer.prepend(reviewCard);
-    
-    textArea.value = ''
+    function saveReview() {
+        localStorage.setItem("review", JSON.stringify(reviews));
+    }
 })
-
-function renderReview(review) {
-    console.log(review)
-}
-
-function saveReview() {
-    localStorage.setItem("review", JSON.stringify(reviews));
-}
