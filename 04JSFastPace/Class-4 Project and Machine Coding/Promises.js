6c881d188ada5869fcbbe1cf9089679571b21215
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-03-02 13:20:53 +0530

    add promisification

--- 04JSFastPace/Class-4 Project and Machine Coding/Promises.js
+++ 04JSFastPace/Class-4 Project and Machine Coding/Promises.js
@@ -3,9 +3,27 @@ console.log(`Starting of Program`);
 // const result = sum(2 ,5);
 // console.log(`Result is: ${result}`);
 
-sum(2, 5, (result) => {
-    console.log(`Result is: ${result}`);
-} )
+// sum(2, 5, (result) => {
+//     console.log(`Result is: ${result}`);
+// } )
+
+// TODO: Promisified
+function sumWithPromise(a, b) {
+    return new Promise((resolve) => {
+        sum(a, b, function (result) {
+            resolve(result)
+        })
+    })
+}
+
+sumWithPromise(3, 4)
+    .then((result) => {
+        console.log(`Promise result is: ${result}`)
+    })
+    .catch((err) => {
+        console.log(`Error is: `, err)
+    })
+
 
 
 function sum(a, b, callback) {
@@ -13,7 +31,6 @@ function sum(a, b, callback) {
     setTimeout(() => {
         callback(a + b)
     }, 5 * 1000);
-    
 }
 
 console.log(`End of Program`);
