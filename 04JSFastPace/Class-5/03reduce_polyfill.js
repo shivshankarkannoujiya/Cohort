437b9bdef9cbcb0968cf32cc4fb1249e5137527d
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-03-03 11:16:52 +0530

    Add v2 of polyfill of reduce

--- 04JSFastPace/Class-5/03reduce_polyfill.js
+++ 04JSFastPace/Class-5/03reduce_polyfill.js
@@ -8,6 +8,27 @@ if (!Array.prototype.myReduce) {
     }
 }
 
+if (!Array.prototype.myReduceV2) {
+    
+    Array.prototype.myReduceV2 = function (callback, initialValue = undefined) {
+
+        if (!initialValue) {
+            let accumulator = this[0]
+            for (let i = 1; i < this.length; i++){
+                accumulator = callback(accumulator, this[i])
+            }
+            return accumulator
+        }
+
+        // if initialValue: given by user
+        let accumulator = initialValue
+        for (let i = 0; i < this.length; i++){
+            accumulator = callback(accumulator, this[i])
+        }
+        return accumulator
+    }
+}
+
 const arr = [1, 2, 3, 4, 5]
-const total = arr.myReduce((acc, currVal) => acc + currVal)
+const total = arr.myReduceV2((acc, currVal) => acc + currVal, 3)
 console.log(total);
