e2864b2c7d2bbf132c9d9e7f7b158c8a7994f697
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-03-03 15:58:00 +0530

    Add throatling

---
+++ 04JSFastPace/Class-5/06Practice.js
@@ -0,0 +1,16 @@
+// TODO: Throatling
+const ptaNhi = (fn, delay) => {
+    let myId = null
+    return (...args) => {
+        if (myId === null) {
+            fn(...args)
+            myId = setTimeout(() => {
+                myId = null
+            }, delay);
+        }
+    }
+}
+
+ptaNhi()
+ptaNhi()
+ptaNhi()
