c81e2b909dcfdf3e1a362786a749287855cfafab
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-03-03 22:22:03 +0530

    Add various fn

---
+++ Practice/JAVASCRIPT/Functions/01Basics.js
@@ -0,0 +1,20 @@
+// Function declaration/statement
+function greet(name) {
+    console.log(`Hello ${name}`)
+}
+
+
+// Function Expression
+const sum = function (a, b) {
+    return a + b
+}
+
+
+// Anonymous Function
+// function(){}
+
+// Named function Expression
+const fn = function abc() {
+    console.log(`This is named function expression`)
+    console.log(abc);
+}
