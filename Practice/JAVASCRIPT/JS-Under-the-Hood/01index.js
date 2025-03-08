ede498b434b1b4859760bc2baf84d27fa90ff0b9
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-03-08 13:40:18 +0530

    Understand the under the hood Working of JS

---
+++ Practice/JAVASCRIPT/JS-Under-the-Hood/01index.js
@@ -0,0 +1,11 @@
+console.log("Hello Ji")
+
+setTimeout(() => {
+    console.log("Execute after 10 Seconds")
+}, 10 * 1000);
+
+console.log("Bye Bye")
+console.log("Bye Bye")
+console.log("Bye Bye")
+console.log("Bye Bye")
+// 3 Million Times (3hrs to Execute)
