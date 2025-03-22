496da2bb42ed1971f2891fa961918c8669e2028f
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-03-22 23:38:26 +0530

    Add custom miniExpress

---
+++ 07Backend_Internals/02Wrapper/miniExpress.js
@@ -0,0 +1,80 @@
+const http = require("http");
+
+function miniExpress() {
+
+    const routes = {
+        GET: {},
+        POST: {}
+    }
+
+    function get(path, handler) {
+        routes.GET[path] = handler
+    }
+
+    function post(path, handler) {
+        routes.POST[path] = handler
+    }
+
+    function handleRequest(req, res) {
+
+        res.statusCode = 200
+
+        res.status = function (statusCode) {
+            res.statusCode = statusCode
+            return res
+        }
+
+        res.send = function (data) {
+            const contentType = typeof data === "object" ? "application/json" : "text/plain" 
+            res.setHeader("Content-Type", contentType);
+            res.end(typeof data === "object" ? JSON.stringify(data) : data);
+        }
+
+        res.json = function (data) {
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(data))
+        }
+
+        // Parse JSON body for POST requests
+        if (req.method === "POST") {
+
+            let body = "";
+            req.on("data", (chunk) => {
+                body+=chunk
+            })
+
+            req.on("end", () => {
+                req.body = body ? JSON.parse(body) : {};
+                processRoute(req, res)
+            })
+        } else {
+            processRoute(req, res);
+        }
+
+    }
+
+    function processRoute(req, res) {
+        const method = req.method;
+        const url = req.url;
+        const routeHandler = routes[method][url];
+
+        if (routeHandler) {
+            routeHandler(req, res);
+        } else {
+            res.status(404).send("404 Not Found")
+        }
+    }
+
+    function listen(port, callback) {
+        const server = http.createServer(handleRequest);
+        server.listen(port, callback)
+    }
+
+    return {
+        get,
+        post,
+        listen
+    }
+}
+
+module.exports = miniExpress
---
+++ 07Backend_Internals/02Wrapper/test.js
@@ -0,0 +1,22 @@
+const miniExpress = require("./miniExpress");
+
+const app = miniExpress();
+const port = 3000
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello from MiniExpress!")
+})
+
+app.post("/data", (req, res) => {
+    res.status(201).json({
+        message: "Resource created successfully"
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+})
+
+
+
+
