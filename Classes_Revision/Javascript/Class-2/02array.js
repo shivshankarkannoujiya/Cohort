bd4f7e6202e2ce0da37361bdce550857142b94fa
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-03-26 15:24:13 +0530

    Add array in js

---
+++ Classes_Revision/Javascript/Class-2/02array.js
@@ -0,0 +1,15 @@
+const fruits = ["apple", "banana", "orange"];
+const intFruits = new Array("Kiwi", "dragon-fruit", "avacado");
+
+// length
+console.log(fruits.length);
+console.log(intFruits.length);
+
+// Accessing elements
+console.log(fruits[0]);
+console.log(intFruits[0]);
+
+// Array methods
+fruits.push("Grapes");
+fruits.unshift("Guavava");
+fruits.pop();
