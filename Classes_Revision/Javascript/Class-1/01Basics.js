29a3fd2d105d0847e9bf0eea8075d2c532e67633
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-03-26 12:43:27 +0530

    Add basics of function

---
+++ Classes_Revision/Javascript/Class-1/01Basics.js
@@ -0,0 +1,24 @@
+function printChai() {
+    console.log(`Hello Js`);
+}
+
+
+function bringBrush(numberOfBrush) {
+    console.log(`Hanji, le aya ${numberOfBrush} brush`);
+}
+
+
+function addTwo(numOne, numTwo) {
+    console.log(`Sum is: ${numOne + numTwo}`);
+}
+
+function addTwoReturn(numOne, numTwo) {
+    return numOne + numTwo
+}
+
+
+// printChai();
+// bringBrush(4)
+// addTwo(4, 6);
+// const result = addTwoReturn(2, 4);
+// console.log(`Result is: ${result}`);
