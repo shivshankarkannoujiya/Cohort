c82d6e1366745e3a7b246c7a243be8dd6454461e
Shivshankar Kannoujiya <kannoujiyaji143@gmail.com>
2025-03-26 14:54:14 +0530

    Add js data-types

---
+++ Classes_Revision/Javascript/Class-2/01bootcampProfile.js
@@ -0,0 +1,34 @@
+// string
+let name = "Rohan";
+
+// Number 24, 23.5
+let age = 25;
+
+// Boolean
+let isPaid = true;
+
+// null: khali/empty
+let favouriteClass = null;
+
+// undefined: Baad me dekhenge
+let homeTown;
+
+// Array
+let skills = ["html", "css", "javascript"];
+
+
+// Object
+let studentProfile = {
+    name: "Rohan",
+    age: 22,
+    isPaid: true,
+    skills: ["html", "css", "javascript"],
+    favouriteClass: null
+}
+
+
+console.log(typeof isPaid);
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof homeTown);
